f267c0f7b25aef99edfbc91a50ea7358771fe7e9
khanh tran <khanh-tran@khanhs-MacBook-Pro.local>
2014-04-13 12:04:19 -0400

    create UIControl subclass

---
+++ KTCustomSlider/KTCustomSlider/KTSliderControl.h
@@ -0,0 +1,13 @@
+//
+//  KTSliderControl.h
+//  KTCustomSlider
+//
+//  Created by khanh tran on 2014-04-13.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTSliderControl : UIControl
+
+@end
---
+++ KTCustomSlider/KTCustomSlider/KTSliderControl.m
@@ -0,0 +1,31 @@
+//
+//  KTSliderControl.m
+//  KTCustomSlider
+//
+//  Created by khanh tran on 2014-04-13.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import "KTSliderControl.h"
+
+@implementation KTSliderControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
