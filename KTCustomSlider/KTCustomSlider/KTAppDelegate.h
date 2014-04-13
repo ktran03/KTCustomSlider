7b5388fcc500799f66480ebaec1c175c37342ff9
khanh tran <khanh-tran@khanhs-MacBook-Pro.local>
2014-04-13 11:57:08 -0400

    base

---
+++ KTCustomSlider/KTCustomSlider/KTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KTAppDelegate.h
+//  KTCustomSlider
+//
+//  Created by khanh tran on 2014-04-13.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KTCustomSlider/KTCustomSliderTests/KTCustomSliderTests.m
@@ -0,0 +1,34 @@
+//
+//  KTCustomSliderTests.m
+//  KTCustomSliderTests
+//
+//  Created by khanh tran on 2014-04-13.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KTCustomSliderTests : XCTestCase
+
+@end
+
+@implementation KTCustomSliderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
