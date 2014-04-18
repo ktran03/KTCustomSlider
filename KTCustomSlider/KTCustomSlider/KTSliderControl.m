38e0b5c8d017b14717e749c25cf187c1b305706f
ktran03 <khanh.tran.vinh@gmail.com>
2014-04-17 21:08:11 -0400

    knob and padding fix

--- KTCustomSlider/KTCustomSlider/KTSliderControl.m
+++ KTCustomSlider/KTCustomSlider/KTSliderControl.m
@@ -53,8 +53,8 @@
 -(void)performInitialization{
     self.backgroundColor = [UIColor clearColor];
     self.relativeControlValue = 30;
-    self.minSliderValue = 0;
-    self.maxSliderValue = 100;
+    self.minSliderValue = 1;
+    self.maxSliderValue = 10;
     self.lastPoint = controlPointXCoord;
     if (!self.barInnerColorLeft) {
         self.barInnerColorLeft = [UIColor yellowColor];
@@ -67,20 +67,6 @@
 }
 
 -(void)configureValueLabels{
-    minLabel = [[UILabel alloc] initWithFrame:CGRectMake(paddingGeneral, barHeightMid, 40, 40)];
-    minLabel.textColor = [UIColor grayColor];
-    minLabel.font = [UIFont fontWithName:@"Helvetica Neue" size:12.0];
-    minLabel.textAlignment = NSTextAlignmentLeft;
-    minLabel.text = [NSString stringWithFormat:@"%d", (int)self.minSliderValue];
-    [self addSubview:minLabel];
-    
-    maxLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.frame.size.width-50.0, barHeightMid, 40, 40)];
-    maxLabel.textColor = [UIColor grayColor];
-    maxLabel.font = [UIFont fontWithName:@"Helvetica Neue" size:12.0];
-    maxLabel.textAlignment = NSTextAlignmentRight;
-    maxLabel.text = [NSString stringWithFormat:@"%d", (int)self.maxSliderValue];
-    [self addSubview:maxLabel];
-    
     currValueLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 40, 40)];
     [currValueLabel setCenter:CGPointMake(controlPointXCoord, barHeightMid)];
     currValueLabel.textColor = [UIColor grayColor];
@@ -92,8 +78,8 @@
 
 -(void)layoutSubviews{
 //    NSLog(@"%f", self.lastPoint);
-//    [currValueLabel setCenter:CGPointMake(self.lastPoint+(knobLengthAndWidth/2), barHeightMid)];
-//    currValueLabel.text = [NSString stringWithFormat:@"%d", (int)convertLastPointToValue];
+//    [currValueLabel setCenter:CGPointMake(self.lastPoint, barHeightMid)];
+//    currValueLabel.text = [NSString stringWithFormat:@"%d", (int)convertLastPointToValue-5];
 }
 
 #pragma mark - UIControl Methods
@@ -109,8 +95,12 @@
     CGPoint lastPointUserTouched = [touch locationInView:self];
     self.lastPoint = lastPointUserTouched.x;
     
-    if (self.lastPoint<=paddingGeneral || self.lastPoint>=(barWidth+paddingGeneral)) {
-        return NO;
+    if (self.lastPoint<=(paddingGeneral)) {
+        self.lastPoint = paddingGeneral;
+    }
+    
+    if (self.lastPoint>=(self.frame.size.width-(paddingGeneral))) {
+        self.lastPoint = self.frame.size.width-(paddingGeneral);
     }
     
     [self sendActionsForControlEvents:UIControlEventValueChanged];
@@ -129,7 +119,7 @@
     
     CGContextRef context = UIGraphicsGetCurrentContext();
     [self drawBar:context];
-    [self drawUndertone:context];
+    //[self drawUndertone:context];
     [self drawControlTriangle:context];
     [self drawKnob:context];
 }
@@ -187,7 +177,7 @@
     {
         CGContextSetShadowWithColor(context, CGSizeMake(0, 0), 2, [UIColor blackColor].CGColor);
         CGRect handleRect = CGRectMake(xCoord,barHeightMid-(knobLengthAndWidth/2), knobLengthAndWidth, knobLengthAndWidth);
-        [[UIColor colorWithWhite:1.0 alpha:0.7]set];
+        [[UIColor colorWithWhite:1.0 alpha:0.4]set];
         CGContextFillEllipseInRect(context, handleRect);
     }
     
